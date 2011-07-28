1ae9982598e926f5f1264a09ed2e3c31a4697830
Caroline Schnapp <mllegeorgesand@gmail.com>
2011-07-27 21:22:26 -0400

    fixed logic... I am tired

--- jquery.cart.js
+++ jquery.cart.js
@@ -39,17 +39,17 @@ Shopify.Cart.ShippingCalculator = (function() {
     var wrapper = jQuery('#' + _config.wrapperId);      
     if (template.length && wrapper.length) {
       template.tmpl(response).appendTo(wrapper);
-    }
-    if (typeof Currency !== 'undefined' && typeof Currency.convertAll === 'function') {
-      var newCurrency = '';
-      if (jQuery('[name=currencies]').size()) {
-        newCurrency = jQuery('[name=currencies]').val();
-      }
-      else if (jQuery('#currencies span.selected').size()) {
-        newCurrency = jQuery('#currencies span.selected').attr('data-currency');
-      }
-      if (newCurrency !== '') {
-        Currency.convertAll(shopCurrency, newCurrency, '#wrapper-response span.money');
+      if (typeof Currency !== 'undefined' && typeof Currency.convertAll === 'function') {
+        var newCurrency = '';
+        if (jQuery('[name=currencies]').size()) {
+          newCurrency = jQuery('[name=currencies]').val();
+        }
+        else if (jQuery('#currencies span.selected').size()) {
+          newCurrency = jQuery('#currencies span.selected').attr('data-currency');
+        }
+        if (newCurrency !== '') {
+          Currency.convertAll(shopCurrency, newCurrency, '#wrapper-response span.money');
+        }
       }
     }
   };  
