dea0390251a30fffe879a6c6cfb0689a0203f09b
Caroline Schnapp <mllegeorgesand@gmail.com>
2011-07-27 20:59:40 -0400

    added compatibility with currencies conversion module

--- jquery.cart.js
+++ jquery.cart.js
@@ -39,7 +39,19 @@ Shopify.Cart.ShippingCalculator = (function() {
     var wrapper = jQuery('#' + _config.wrapperId);      
     if (template.length && wrapper.length) {
       template.tmpl(response).appendTo(wrapper);
-    }    
+    }
+    if (typeof Currency !== 'undefined' && typeof Currency.convertAll === 'function') {
+      var newCurrency = '';
+      if (jQuery('[name=currencies]').size()) {
+        newCurrency = jQuery('[name=currencies]').val();
+      }
+      else if (jQuery('#currencies span.selected').size()) {
+        newCurrency = jQuery('#currencies span.selected').attr('data-currency');
+      }
+      if (newCurrency !== '') {
+        Currency.convertAll(shopCurrency, newCurrency, '#wrapper-response span.money');
+      }
+    }
   };  
   var _enableButtons = function() {
    jQuery('.get_rates').removeAttr('disabled').removeClass('disabled').val(_config.submitButton);
